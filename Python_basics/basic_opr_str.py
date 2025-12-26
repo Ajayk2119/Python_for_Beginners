df99807452cbda9c0d5d8c0f4c423831fe540f7c
ajayk2119@gmail.com <ajayk2119@gmail.com>
2025-12-26 14:51:52 +0530

    day-1 practice

---
+++ Python_basics/basic_opr_str.py
@@ -0,0 +1,7 @@
+a = 'Hello World'
+b = "Hi How Are You"
+c = """ I am fine
+how about you """
+
+#len is used to calculate length of string
+print (len(a), len(b), len(c), a, b, c)
---
+++ Python_basics/variable_datatype.py
@@ -0,0 +1,12 @@
+# Variable is the memory location to store the value, in below examples we will store different type data type into variables
+a = 10   # int type
+b = 12.10  # float type
+c = "My data" # string type
+d = True # boolean type
+e = [1, 2, 3, 4] #list type
+f = (1, 2, 3) #tuple type
+g = {"a": 2}  # dict type
+
+print(type(a), type(b), type(c), type(d), type(e), type(f), type(g))
+
+print(a, b, c, d, e, f, g)
